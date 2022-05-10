b2b4f92012bf1c276fa71a18b869dcf37aaa5214
BairSultumov <sultumovb@gmail.com>
2022-05-10 19:39:48 +0800

    beta ver

--- website/app.js
+++ website/app.js
@@ -5,6 +5,9 @@ const fs = require("fs");
 const expressHbs=require("express-handlebars");
 const path = require('path');
 const bodyParser=require("body-parser");
+const multer=require("multer");
+const urlencoded = require("body-parser/lib/types/urlencoded");
+const upload=multer();
 const urlencodedParser=bodyParser.urlencoded({extended:false});
 var arr=JSON.parse(fs.readFileSync("userTable.json", "utf8"));
 
@@ -12,9 +15,14 @@ app.set("view options", { layout: "layouts/layout" });
 app.set("view engine","hbs");
 hbs.registerPartials(path.join(__dirname+"/views/partials"));
 
-var Data = new Date();
 app.use(function(request,response,next){
-    console.log('Запрошенный адрес:'+ request.url+'\t'+Data);
+  let gettime = function () {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    return `${hour}:${minute}`
+  }
+  console.log(`${gettime()}: Запрошенный адрес: ${request.url}`);
     next();
 });//Логи
 
@@ -67,15 +75,39 @@ app.get("/survival",urlencodedParser,function(request,response){
   response.render("survival");
 });
 
+app.post("/getData", upload.fields([]), (request, response) => {
+  if (!request.body)
+      return response.sendStatus(400);
+  if (request.body.log == "user" && request.body.pas == "user")
+      response.send("Страница пользователя");
+  else if (request.body.log == "admin" && request.body.pas == "admin")
+      response.send("Страница администратора");
+  else
+      response.send("");
+});
+
+// function createTable(arr){
+//   let data="<tr>";
+//   arr.forEach(element => {
+//     data+=`<td>${element.name}</td><td>${element.description}`
+//   });
+//   data+="</td>"
+//   return data;
+// }
+
 app.post("/MainUserTable",urlencodedParser,function(request,response){
     if(!request.body) return response.sendStatus(400);
     else{
+      let data;
+      console.log("/MainUserTable  РАБОТАЕТ!!!!!!!!!!!!!!!!!!!")
         arr=JSON.parse(fs.readFileSync("userTable.json", "utf8"));
         arr.push({name:request.body.name, description:request.body.description});
+        data.push({name:request.body.name, description:request.body.description});
         fs.writeFile("userTable.json", JSON.stringify(arr), (error) =>{
             if(error) throw error;
             console.log("Запись файла завершена.");
         });
+        response.send(data);
     }
  });
 
